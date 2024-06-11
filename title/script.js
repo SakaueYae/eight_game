404d4facbcf9d02602fec0bc53485c7a46eb5865
nagao <shoushou2353@gmail.com>
2024-06-11 18:53:54 +0900

    title

--- title/script.js
+++ title/script.js
@@ -9,4 +9,9 @@ $(document).ready(function() {
         alert('レベル ' + level + ' が選択されました!');
         // ここにゲームのレベルに応じた処理を追加します。
     });
+
+    $('.Description').click(function(){
+        alert("ゲームの説明が選択されました");
+        // 説明の画面
+    })
 });
