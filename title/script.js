20606ddc025830fa64493a0519eeae7b786360e7
SakaueYae <y.sakaue@4nonome.com>
2024-06-14 12:01:15 +0900

    ホーム画面からの遷移先指定

--- title/script.js
+++ title/script.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
-    $('#main-title-screen').click(function() {
-        $(this).hide();
-        $('#level-selection-screen').show();
-    });
+$(document).ready(function () {
+  $("#main-title-screen").click(function () {
+    $(this).hide();
+    $("#level-selection-screen").show();
+  });
 
-    $('.level-button').click(function() {
-        var level = $(this).data('level');
-        alert('レベル ' + level + ' が選択されました!');
-        // ここにゲームのレベルに応じた処理を追加します。
-    });
+  $(".level-button").click(function () {
+    var level = $(this).data("level");
+    localStorage.setItem("level", level);
+    window.location.href = "../progress/8game_sample3.html";
+    // ここにゲームのレベルに応じた処理を追加します。
+  });
 
-    $('.Description').click(function(){
-        $('#level-selection-screen').hide();
-        $('#description-screen').show();
-        // 説明の画面
-    })
+  $(".Description").click(function () {
+    $("#level-selection-screen").hide();
+    $("#description-screen").show();
+    // 説明の画面
+  });
 
-    $('.return-level-selection').click(function(){
-        $('#description-screen').hide();
-        $('#level-selection-screen').show();
-        // レベル選択に戻る
-    })
+  $(".return-level-selection").click(function () {
+    $("#description-screen").hide();
+    $("#level-selection-screen").show();
+    // レベル選択に戻る
+  });
 });
